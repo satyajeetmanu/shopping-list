c2fc6e12d6753281dbff03fa00453a8781aa7fc6
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-10-28 12:56:06 +0530

    added a new item screen and the required navigation

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => const NewItem(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addItem,
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text('Grocery List'),
       ),
