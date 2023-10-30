65d4b2cd27f8bc352bdeb704ec9dfafb54cc7e48
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2023-10-30 11:49:15 +0530

    added validation and reset logic using a global key as the form key

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -9,6 +9,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,62 +24,85 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              maxLength: 50,
-              decoration: const InputDecoration(
-                label: Text('Name'),
-              ),
-              validator: (value) {
-                return 'demo...';
-              },
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Expanded(
-                  child: TextFormField(
-                    initialValue: '1',
-                    decoration: const InputDecoration(label: Text('Quantity')),
+                TextFormField(
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    label: Text('Name'),
                   ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return 'Must be between 2 and 50 characters long.';
+                    }
+                    return null;
+                  },
                 ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: DropdownButtonFormField(
-                    items: [
-                      for (final category in categories.entries)
-                        DropdownMenuItem(
-                          value: category.value,
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 16,
-                                width: 16,
-                                color: category.value.color,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              int.tryParse(value) == null ||
+                              int.tryParse(value)! <= 0) {
+                            return 'Must be a valid, positive number';
+                          }
+                          return null;
+                        },
+                        initialValue: '1',
+                        keyboardType: TextInputType.number,
+                        decoration:
+                            const InputDecoration(label: Text('Quantity')),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 16,
+                                    width: 16,
+                                    color: category.value.color,
+                                  ),
+                                  const SizedBox(width: 6),
+                                  Text(category.value.title),
+                                ],
                               ),
-                              const SizedBox(width: 6),
-                              Text(category.value.title),
-                            ],
-                          ),
-                        )
-                    ],
-                    onChanged: (value) {},
-                  ),
+                            )
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          _formKey.currentState!.reset();
+                        },
+                        child: const Text('Reset')),
+                    const SizedBox(width: 4),
+                    ElevatedButton(
+                        onPressed: _saveItem, child: const Text('Save Item'))
+                  ],
                 )
               ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(onPressed: () {}, child: const Text('Reset')),
-                const SizedBox(width: 4),
-                ElevatedButton(onPressed: () {}, child: const Text('Save Item'))
-              ],
-            )
-          ],
-        )),
+            )),
       ),
     );
   }
